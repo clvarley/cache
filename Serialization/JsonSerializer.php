e09c51c4e2bb0612219d8d8ae988e5f324ac8d64
clvarley <conorlukevarley@gmail.com>
2021-05-06 21:56:31 +0100

    PHP and JSON serializers

--- Serialization/JsonSerializer.php
+++ Serialization/JsonSerializer.php
@@ -1,47 +1,45 @@
 <?php
 
-namespace Cache\Serialization;
+namespace Cache;
 
 use Cache\SerializerInterface;
 use Cache\CacheItem;
 
-use function serialize;
-use function unserialize;
+use function json_encode;
+use function json_decode;
 
 /**
- * Serializes cache items using normal PHP serialization
+ * Serializes cache items into JSON
  *
  * @package Cache
  * @author clvarley
  */
-Class PhpSerializer Implements SerializerInterface
+Class JsonSerializer Implements SerializerInterface
 {
 
     /**
-     * Serialize the cache item
+     * Serialize the cache item into JSON
      *
      * @param CacheItem $item Cache item
      * @return string         Serialized item
      */
     public function serialize( CacheItem $item ) : string
     {
-        return serialize( $item );
+        // TODO:
+
+        return '';
     }
 
     /**
-     * Deserialize into a cache item
+     * Deserialize JSON into a cache item
      *
      * @param string $serialized Serialized item
      * @return CacheItem         Cache item
      */
     public function deserialize( string $serialized ) : CacheItem
     {
-        $item = unserialize( $serialized );
+        // TODO:
 
-        if ( !$item instanceof CacheItem ) {
-            // TODO: Define custom exceptions
-        }
-
-        return $item;
+        return new CacheItem;
     }
 }
