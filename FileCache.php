47a3faefc4aaf632abf47f36addd4c06c8eeb6b1
clvarley <conorlukevarley@gmail.com>
2021-05-06 21:41:51 +0100

    Added static function to create default file cache

--- FileCache.php
+++ FileCache.php
@@ -5,6 +5,8 @@ namespace Cache;
 use Cache\CacheInterface;
 use Cache\SerializerInterface;
 use Cache\HashInterface;
+use Cache\Hashing\Md5Adapter;
+use Cache\Serialization\PhpSerializer;
 
 /**
  * File backed caching system
@@ -16,25 +18,52 @@ Class FileCache Implements CacheInterface
 {
 
     /**
+    *
+    *
+    * @var string $directory Root directory
+    */
+    protected $directory;
+
+    /**
+     *
+     *
      * @var SerializerInterface $serializer Value serializer
      */
     protected $serializer;
 
     /**
+     *
+     *
      * @var HashInterface $hasher Hash adapter
      */
     protected $hasher;
 
     /**
-     * @var string $directory Root directory
+     * Create a file cache using the serialization and hashing method provided
+     *
+     * @param string $directory               Root directory
+     * @param SerializerInterface $serializer Serialization method
+     * @param HashInterface $hasher           Hashing method
      */
-    protected $directory;
+    public function __construct( string $directory, SerializerInterface $serializer, HashInterface $hasher )
+    {
+        $this->directory = $directory;
+        $this->serializer = $serializer;
+        $this->hasher = $hasher;
+    }
 
     /**
+     * Creates a FileCache with sensible defaults
      *
+     * @param string $directory Root directory
+     * @return static           FileCache adapter
      */
-    public function __construct()
+    public static function create( string $directory ) : FileCache
     {
-
+        return new static(
+            $directory,
+            new Md5Adapter,
+            new PhpSerializer
+        );
     }
 }
---
+++ Serialization/PhpSerializer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Cache\Serialization;
+
+use Cache\SerializerInterface;
+
+/**
+ * Serializes cache items using normal PHP serialization
+ *
+ * @package Cache
+ * @author clvarley
+ */
+Class PhpSerializer Implements SerializerInterface
+{
+
+}
