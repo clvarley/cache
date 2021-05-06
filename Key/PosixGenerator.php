7a91c69e72e6642d94ed3aac9bd9ad0e38164512
clvarley <conorlukevarley@gmail.com>
2021-05-06 23:13:15 +0100

    Created POSIX safe key generator

---
+++ Key/PosixGenerator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cache\Key;
+
+use Cache\KeyGeneratorInterface;
+
+use preg_replace;
+
+/**
+ * Makes keys safe according to the POSIX portable filename standard
+ *
+ * @package Cache
+ * @author clvarley
+ */
+Class PosixGenerator Implements KeyGeneratorInterface
+{
+
+    /**
+     * Strips out non-POSIX portable filename characters
+     *
+     * @param string $subject Subject string
+     * @return string         Key
+     */
+    public function hash( string $subject ) : string
+    {
+        return preg_replace( '/[A-Za-z0-9_\-\.]/', '', $subject );
+    }
+}
