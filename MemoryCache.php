2791b73896f0e4d3b8db4988bd83848c305cf5f0
clvarley <conorlukevarley@gmail.com>
2021-05-07 19:03:29 +0100

    Created the most basic in memory store

---
+++ MemoryCache.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cache;
+
+use Cache\CacheInterface;
+use Cache\CacheItem;
+
+use const PHP_INT_MAX;
+
+/**
+ * Very simple in memory cache store
+ *
+ * @package Cache
+ * @author clvarley
+ */
+Class MemoryCache Implements CacheInterface
+{
+
+    /**
+     *  Currently stored items
+     *
+     * @var CacheItem[] $items Cache items
+     */
+    private $items = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get( string $key ) /* : ?mixed */
+    {
+        if ( isset( $this->items[$key] ) && $this->items[$key]->isValid() ) {
+            return $this->items[$key];
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set( string $key, /* mixed */ $value, int $lifetime = 0 ) : void
+    {
+        $this->items[$key] = new CacheItem( $value, $lifetime );
+    }
+}
