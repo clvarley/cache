b5b5f9a3b8d2d22110ef8e895cb58eb1e034c07b
clvarley <conorlukevarley@gmail.com>
2021-05-07 17:19:08 +0100

    Added a basic 'raw' key generator

---
+++ Key/RawGenerator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cache\Key;
+
+use Cache\KeyGeneratorInterface;
+
+/**
+ * Just returns the raw text for use as the key
+ *
+ * Useful for easy refactoring or if you want to be able to control your cache
+ * keys directly, without them being run through a hashing algorithm.
+ *
+ * @package Cache
+ * @author clvarley
+ */
+Class RawGenerator Implements KeyGeneratorInterface
+{
+
+    /**
+     * Simply return the subject as the key
+     *
+     * @param string $subject Subject string
+     * @return string         Subject string
+     */
+    public function hash( string $subject ) : string
+    {
+        return $subject;
+    }
+}
