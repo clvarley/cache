6f1e49daae35d31cabb356c982d5f32cdfff25f3
clvarley <conorlukevarley@gmail.com>
2021-05-09 02:33:08 +0100

    Added crc32 based key generator

---
+++ src/Key/Crc32Generator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Clvarley\Cache\Key;
+
+use Clvarley\Cache\KeyGeneratorInterface;
+
+use function hash;
+
+/**
+ * CRC32 based key generator
+ *
+ * @package Cache
+ * @author clvarley
+ */
+Class Crc32Generator Implements KeyGeneratorInterface
+{
+
+    /**
+     * Creates a key using the CRC32 algorithm
+     *
+     * @param string $subject Subject string
+     * @return string         CRC32 key
+     */
+    public function generate( string $subject ) : string
+    {
+        return hash( 'crc32', $subject );
+    }
+}
