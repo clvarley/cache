fca10366c00537b22bbb1771293ef045a262d916
clvarley <conorlukevarley@gmail.com>
2021-07-21 01:07:36 +0100

    Fleshed out JSON tests

--- tests/Serialization/JsonSerializer.php
+++ tests/Serialization/JsonSerializer.php
@@ -13,4 +13,96 @@ use stdClass;
 Class JsonSerializerTest Extends AbstractSerializerTest
 {
 
+    /**
+     * Make sure the JsonSerializer can serialize a string
+     */
+    public function testSerializeString()
+    {
+        $item = $this->createItem( "testString" );
+
+        $serializer = new JsonSerializer();
+        $serialized = $serializer->serialize( $item );
+
+        $this->assertEquals( $serialized, "" ); // TODO: get serialized value
+
+        return $serialized;
+    }
+
+    /**
+     * Make sure the JsonSerializer can deserialize a string
+     *
+     * @depends testSerializeString
+     */
+    public function testDeserializeString( string $serialized )
+    {
+        $serializer = new JsonSerializer();
+        $item = $serializer->deserialize( $serialized );
+
+        $this->assertEquals(          $item->value,  "testString"  );
+        $this->assertEqualsWithDelta( $item->expires, $this->start_time, 1 );
+    }
+
+    /**
+     * Make sure the JsonSerializer can serialize an array
+     */
+    public function testSerializeArray()
+    {
+        $item = $this->createItem( [ 1, 2, 3 ] );
+
+        $serializer = new JsonSerializer();
+        $serialized = $serializer->serialize( $item );
+
+        $this->assertEquals( $serialized, "" ); // TODO: get serialized value
+
+        return $serialized;
+    }
+
+    /**
+     * Make sure the JsonSerializer can deserialize an array
+     *
+     * @depends testSerializeArray
+     */
+    public function testDeserializeArray( string $serialized )
+    {
+        $serializer = new JsonSerializer();
+        $item = $serializer->deserialize( $serialized );
+
+        $this->assertEquals(          $item->value,  [ 1, 2, 3 ]  );
+        $this->assertEqualsWithDelta( $item->expires, $this->start_time, 1 );
+    }
+
+    /**
+     * Make sure the JsonSerializer can serialize an object
+     */
+    public function testSerializeObject()
+    {
+        $object = new stdClass;
+        $object->id = 123;
+        $object->prop = "test";
+
+        $item = $this->createItem( $object );
+
+        $serializer = new JsonSerializer();
+        $serialized = $serializer->serialize( $item );
+
+        $this->assertEquals( $serialized, "" ); // TODO: get serialized value
+
+        return $serialized;
+    }
+
+    /**
+     * Make sure the JsonSerializer can deserialize an object
+     *
+     * @depends testSerializeObject
+     */
+    public function testDeserializeObject( string $serialized )
+    {
+        $serializer = new JsonSerializer();
+        $item = $serializer->deserialize( $serialized );
+
+        $this->assertInstanceOf( stdClass::class, $item->value );
+        $this->assertEquals( $item->value->id,   123 );
+        $this->assertEquals( $item->value->prop, "test" );
+        $this->assertEqualsWithDelta( $item->expires, $this->start_time, 1 );
+    }
 }
