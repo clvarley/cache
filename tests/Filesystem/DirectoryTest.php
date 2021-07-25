8b9883b9f698be9971ed89f6324fcf365b2ebbfd
clvarley <conorlukevarley@gmail.com>
2021-07-25 18:28:50 +0100

    Stubbed out tests for filesystem classes

---
+++ tests/Filesystem/DirectoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Clvarley\Cache\Tests\Filesystem;
+
+use PHPUnit\Framework\TestCase;
+use Clvarley\Cache\Filesystem\Directory;
+
+Class DirectoryTest Extends TestCase
+{
+    
+}
---
+++ tests/Filesystem/FileTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Clvarley\Cache\Tests\Filesystem;
+
+use PHPUnit\Framework\TestCase;
+use Clvarley\Cache\Filesystem\File;
+
+Class FileTest Extends TestCase
+{
+
+}
