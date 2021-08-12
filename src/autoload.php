2c86586866890ef5dafe0ed420eb6151f8530bd8
clvarley <conorlukevarley@gmail.com>
2021-08-12 13:34:54 +0100

    Updated comment

--- src/autoload.php
+++ src/autoload.php
@@ -1,12 +1,20 @@
 <?php
-
 /**
  * Basic autoloader for developers not using composer
  *
  * While the preferred method of using this library is to install and autoload
- * via Composer, sometimes it can be helpfull to just include the files
- * directly.
+ * via Composer, sometimes it can be helpful to just include the files directly.
+ *
+ * ```php
+ * // Replace '...' with correct path
+ * require_once '.../src/autoload.php';
+ * ```
+ *
+ * @package Cache
+ * @author clvarley
  */
+
+// Register our custom handler
 spl_autoload_register( function ( string $classname ) : void {
     // Not one of ours
     if ( substr( $classname, 0, 14 ) !== 'Clvarley\Cache' ) {
