81eac0eb9a6fc883b8f35c87871c3aadf3b57270
clvarley <conorlukevarley@gmail.com>
2021-08-19 17:43:40 +0100

    Stubbed out tests for the FileCache

---
+++ tests/FileCacheTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Clvarley\Cache\Tests;
+
+use Clvarley\Cache\Tests\Filesystem\AbstractFilesystemTest;
+use Clvarley\Cache\FileCache;
+use stdClass;
+
+use function sleep;
+
+/**
+ * @group Caches
+ */
+Class FileCacheTest Extends AbstractFilesystemTest
+{
+
+    /**
+     * Test object for equality check
+     *
+     * @param stdClass $test_obj
+     */
+    private $test_obj;
+
+    /**
+     * Creates a new file cache with the given lifetime
+     *
+     * @param int $lifetime Cache lifetime
+     * @return FileCache    Cache store
+     */
+    private function createCache( int $lifetime ) : FileCache
+    {
+        $this->test_obj = new stdClass;
+        $this->test_obj->id = 123;
+        $this->test_obj->prop = "test";
+
+        $cache = new FileCache();
+
+        $cache->set( "testObj",    $this->test_obj, $lifetime );
+        $cache->set( "testString", "123",           $lifetime );
+        $cache->set( "testInt",    123,             $lifetime );
+        $cache->set( "testArr",    [ 1, 2, 3 ],     $lifetime );
+
+        return $cache;
+    }
+
+    /**
+     * Make sure items can be read from the cache
+     */
+    public function testDoesReturnValidItems()
+    {
+        $lifetime = 1; // 1 second
+
+        $cache = $this->createCache( $lifetime );
+
+        $this->assertSame(   $cache->get( "testObj" ),    $this->test_obj );
+        $this->assertEquals( $cache->get( "testString" ), "123" );
+        $this->assertEquals( $cache->get( "testInt" ),    123 );
+        $this->assertEquals( $cache->get( "testArr" ),    [ 1, 2, 3 ] );
+    }
+
+    /**
+     * Make sure permanent cache items persist
+     */
+    public function testDoesReturnPermanentItems()
+    {
+        $lifetime = 0; // No expiry
+
+        $cache = $this->createCache( $lifetime );
+
+        $this->assertSame(   $cache->get( "testObj" ),    $this->test_obj );
+        $this->assertEquals( $cache->get( "testString" ), "123" );
+        $this->assertEquals( $cache->get( "testInt" ),    123 );
+        $this->assertEquals( $cache->get( "testArr" ),    [ 1, 2, 3 ] );
+    }
+
+    /**
+     * Make sure items in the cache expire
+     */
+    public function testDoesNotReturnExpiredItems()
+    {
+        $lifetime = 1; // 1 second
+
+        $cache = $this->createCache( $lifetime );
+
+        sleep( $lifetime ); // Force expiry
+
+        $this->assertNull( $cache->get( "testObj" ) );
+        $this->assertNull( $cache->get( "testString" ) );
+        $this->assertNull( $cache->get( "testInt" ) );
+        $this->assertNull( $cache->get( "testArr" ) );
+    }
+}
