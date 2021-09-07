5655e49d237117cabd8d517e036531c444fb14cc
clvarley <conorlukevarley@gmail.com>
2021-09-07 18:58:20 +0100

    Test json encoding flags are respected and exceptions thrown

--- tests/Serialization/JsonSerializerTest.php
+++ tests/Serialization/JsonSerializerTest.php
@@ -4,11 +4,13 @@ namespace Clvarley\Cache\Tests\Serialization;
 
 use Clvarley\Cache\Tests\Serialization\AbstractSerializerTest;
 use Clvarley\Cache\Serialization\JsonSerializer;
+use Clvarley\Cache\Exception\DeserializationException;
 use stdClass;
+use Closure;
 
 use function json_encode;
 
-use const JSON_PRESERVE_ZERO_FRACTION;
+use const JSON_PRETTY_PRINT;
 
 /**
  * @group Serialization
@@ -117,5 +119,85 @@ Class JsonSerializerTest Extends AbstractSerializerTest
         $this->assertEquals( $item->value->id,   123 );
         $this->assertEquals( $item->value->prop, "test" );
         $this->assertEqualsWithDelta( $item->expires, self::$start_time, 1 );
+    }
+
+    /**
+     * Make sure the JsonSerializer can have custom encoding flags
+     */
+    public function testCanSetEncodingFlags()
+    {
+        $serializer = new JsonSerializer();
+        $serializer->setEncoding( 0 | JSON_PRETTY_PRINT );
+
+        // Give the tests access to the private properties
+        $tests = Closure::bind(
+            function ( JsonSerializer $serializer ) {
+                $this->assertEquals( $serializer->encoding, JSON_PRETTY_PRINT );
+            },
+            $this,
+            JsonSerializer::class
+        );
+        $tests( $serializer );
+
+        return $serializer;
+    }
+
+    /**
+     * Make sure the JsonSerializer respects custom encoding flags
+     *
+     * @depends testCanSetEncodingFlags
+     */
+    public function testUsesEncodingFlags( JsonSerializer $serializer )
+    {
+        $object = new stdClass;
+        $object->id = 123;
+        $object->prop = "test";
+
+        $item = $this->createItem( $object );
+
+        $serialized = $serializer->serialize( $item );
+
+        $this->assertEquals(
+            $serialized,
+            <<<JSON
+            {
+                "value": {
+                    "id": 123,
+                    "prop": "test"
+                },
+                "expires": {$item->expires}
+            }
+            JSON
+        );
+    }
+
+    /**
+     * Make sure the JsonSerializer throws on invalid strings
+     */
+    public function testThrowsOnInvalidStrings()
+    {
+        $serializer = new JsonSerializer();
+
+        // Try to deserialize a invalid value
+        try {
+            $serializer->deserialize( '@@some_invalid_value' );
+        } catch ( DeserializationException $e ) {}
+
+        $this->assertInstanceOf( DeserializationException::class, $e );
+    }
+
+    /**
+     * Make sure the JsonSerializer throws on invalid classes
+     */
+    public function testThrowsOnInvalidClasses()
+    {
+        $serializer = new JsonSerializer();
+
+        // Try to deserialize a non-CacheItem class
+        try {
+            $serializer->deserialize( '{"id":123}' );
+        } catch ( DeserializationException $e ) {}
+
+        $this->assertInstanceOf( DeserializationException::class, $e );
     }
 }
